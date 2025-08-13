02f1a567bb1ce33c9e4a3e6f5c00299a6582878e
Mauro Borgeaud <mauroborgeaud1@Outlook.com>
2025-08-13 19:00:40 -0300

    Update de puntos 1 y 3
    Update: Cambio var por let.       Removed: showAlertError

--- js/validacion.js
+++ js/validacion.js
@@ -6,19 +6,22 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
- function validateForm() {
-    var nombre = document.getElementById("nombre").value;
-    var apellido = document.getElementById("apellido").value;
-    var email = document.getElementById("email").value;
-    var contraseña = document.getElementById("password1").value;
-    var contraseña2 = document.getElementById("password2").value;
+function validateForm() {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let email = document.getElementById("email").value;
+    let contraseña = document.getElementById("password1").value;
+    let contraseña2 = document.getElementById("password2").value;
+
+
+    // Validación de campos vacíos y coincidencia de contraseñas
 
     if (nombre === "" || apellido === "" || email === "" || contraseña === "" || contraseña2 === "") {
-        showAlertError();
+        
         return false;
     }
     if (contraseña !== contraseña2) {
-        showAlertError();
+        
         return false;
     }
- }
+}

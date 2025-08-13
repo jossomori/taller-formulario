dad8e4e69d2fe625992ca879c0aa1e41f3ea90ed
Josué Sosa <05josuesosa@gmail.com>
2025-08-13 19:00:12 -0300

    feat: validar que el campo de contraseña no esté vacío

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,27 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+// Actividad 7.3:
+function validateForm() {
+    let passInput = document.getElementById("password1");
+
+    // Validar que el campo de contraseña no esté vacío
+    if (passInput.value.length < 6) {
+        return false;
+    }
+    else {
+        return true;
+    }
+} 
+
+const submitButton = document.getElementById('regBtn');
+
+submitButton.addEventListener('click', function() {
+    if (validateForm()) {
+        showAlertSuccess();
+    }
+    else {
+        showAlertError();
+    }
+})

22102083904f9cc3de14aeb76c86ab6b7d2829de
Mauro Borgeaud <mauroborgeaud1@Outlook.com>
2025-08-13 18:41:46 -0300

    Update validacion.js
    Ningún campo puede estar vacío.
    Los datos ingresados en "Contraseña" y "Repetir contraseña" deben ser iguales.

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,20 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+ function validateForm() {
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var email = document.getElementById("email").value;
+    var contraseña = document.getElementById("password1").value;
+    var contraseña2 = document.getElementById("password2").value;
+
+    if (nombre === "" || apellido === "" || email === "" || contraseña === "" || contraseña2 === "") {
+        showAlertError();
+        return false;
+    }
+    if (contraseña !== contraseña2) {
+        showAlertError();
+        return false;
+    }
+ }
